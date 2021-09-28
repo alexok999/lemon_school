54d379351edf1fed8edcbdd21ee467b197b38a86
Alexok <16san4ez@gmail.com>
2021-09-28 09:29:45 +0300

    start do to homework

---
+++ script.js
@@ -0,0 +1,259 @@
+// let a = 5;
+// if (a > 1){
+//     console.log('Больше');
+// }
+
+
+// let age = 13;
+// if (age > 17) {
+//     console.log("18+");
+// } else {
+//     console.log("18-");
+// }
+
+// let c = 4;
+// if (c === 1){
+//     console.log('odin0');
+// } else  if ( c === 2){
+//     console.log ("dva");
+// } else if (c === 3){
+//     console.log("tri");
+// } else {
+//     console.log("ne 1 ne 2 ne 3");
+// }
+
+
+// let age = 13;
+// if (age > 17) {
+//     console.log("18+");
+// } else {
+//     console.log("18-");
+// }
+
+
+
+// Условие ? дейтвие если true и действие если false
+// let age = 17;
+// (age >= 18) ? console.log('bolshe ili ravno') : (age === 17) ? console.log('17') : console.log('ne 17');
+
+
+// let b = ( 5 > 4) ? 3 : 2;
+// console.log(( 5 > 4) ? 'boslhe' : 'net');
+
+/////////
+// let count = 12;
+// switch(count){
+//     case 1:
+//         console.log('count is 1');
+//         break;
+//     case 2:
+//         console.log('count is 2');
+//         break;
+//     case 3:
+//         console.log('count is 3');
+//         break;
+//     default:
+//         console.log('default');
+
+// }
+
+
+
+// let greed = 'Привет';
+// switch (greed){
+//     case 'Привет':
+//         console.log('Вежливость 0');
+//         break;
+//     case 'Здравствуйте':
+//         console.log('Вежливость 100');
+//         break;
+//     default:
+//         console.log('не поздоровался');
+// }
+
+// let count2 = 14;
+// switch (count2){
+//     case (14 || 13 || 15):
+//         console.log('24 || 15 || 13');   
+//         break;
+//     default:
+//         console.log('ss');
+// }
+
+
+
+//Циклы  for  - for in - for of -- while -- do while
+//
+// let a = 10;
+// while (a >= 5){
+//     console.log(a);
+//     a--;    
+// }
+
+
+
+
+
+// let counter = 0;
+// while(true){
+//     console.log('1');
+//     counter++;
+
+//     if(counter === 5){
+//         break;
+//     }
+// }
+
+
+// let counter2 = 0;
+// do{
+//     console.log('doing');
+//     counter2++;
+// } while (counter2 < 5);
+
+
+
+// let counter3 = 2;
+// do{
+//     console.log(counter3);
+// } while( counter3 !== 2);
+
+
+// // начало итерации; условие пока идет работа цикла; действие которое не всегда обязательное
+// for(let i = 0; i<=10; i++){
+//     console.log('forin');
+// }
+
+// let age = 17;
+// // > < == === >= <=
+// // if else
+// if (age >= 18) {
+//     console.log("Совершенолетний")
+// } else {
+//     console.log("не совершенолетний")
+// }
+//
+//
+// let a = 2;
+//
+// if (a === 1) {
+//     console.log('odin');
+// } else if (a === 2) {
+//     console.log('dva');
+// } else if (a === 3) {
+//     console.log('three');
+// } else {
+//     console.log('NE ODIN NE DVA NE THREE');
+// }
+//
+// if (a === 1) {
+//
+// }
+//
+// if (a === 2) {
+//
+// }
+//
+//
+//
+// if (age >= 18) {
+//     console.log("Совершенолетний")
+// } else {
+//     console.log("не совершенолетний")
+// }
+//
+// // (условие) ? действие если true : действие если false
+//
+// (age >= 18) ? console.log('bolshe ili ravno')
+//     : (age === 17)
+//     ? console.log('17')
+//     : console.log('ne 17');
+//
+//
+// let b = (5 > 4) ? 3 : 2;
+// console.log((5 > 4) ? 'bolshe' : 'net');
+//
+//
+// // switch case
+// let count = 20;
+// switch(count) {
+//     case 20:
+//         console.log('count is 20 num');
+//         break;
+//     case 1:
+//        console.log('count is 1');
+//        break;
+//     case 2:
+//         console.log('count is 2');
+//         break;
+//     case 3:
+//         console.log('count is 3');
+//         break;
+//     default:
+//         console.log('default');
+// }
+//
+// let greet = 'Здравствуйте';
+// switch (greet) {
+//     case 'Привет':
+//         console.log('Вежливость 0');
+//         break;
+//     case 'Здравствуйте':
+//         console.log('Вежливость 100');
+//         break;
+//     default:
+//         console.log('Не поздоровался');
+// }
+//
+// let count2 = 16;
+//
+// switch (count2) {
+//     case 14:
+//     case 13:
+//     case 15:
+//         console.log('14 || 13 || 15');
+//         break;
+// }
+//
+//
+// // for .  for in  .  for of  .  while    do while
+// // c = c + 1 : c++   c = c - 1: c--   // следующее обращение к переменной даст +1
+// // c = c + 5 : c += 5   c = c - 5 : c -= 5;
+// // ++c   --c  // текущее обращение к переменной даст +1
+//
+// // let counter = 0;
+// // while (true) {
+// //     console.log('1');
+// //     counter++;
+// //
+// //     if (counter === 5) {
+// //         break;
+// //     }
+// }
+//
+//
+
+// console.log('\n\n')
+//
+// // let counter2 = 0;
+// // do {
+// //     console.log('doing');
+// //     counter2++;
+// // } while (counter2 < 5)
+//
+// let counter = 2;
+//
+// do {
+//     console.log(counter);
+// } while (counter !== 2);
+
+// for (начало итерации; условие пока цикл работает; действие на каждой итерации) {  действие }
+
+
+// for(let i = 0; i <= 5; i++) {
+//     let count = 0;
+//     console.log('foring');
+//     count++;
+// }
+
+
